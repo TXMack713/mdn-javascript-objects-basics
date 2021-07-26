4355c96659a4b0f50311fe11bbb32274cb2e7522
TXMack713 <mackattack713@gmail.com>
2021-07-26 13:54:36 -0500

    3 files added, 1 deleted

---
+++ assessments.js
@@ -0,0 +1,48 @@
+// MDN JavaScript Objects Basics Assessmnet 1
+let cat = {
+  name: 'Bertie',
+  breed: 'Cymric',
+  color: 'white',
+  greeting: function() {
+    console.log('Meow!');
+  }
+};
+
+// Put your code here
+let catName;
+catName = cat['name'];
+cat.greeting();
+cat.color = 'black';
+
+// Don't edit the code below here
+
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+
+para1.textContent = `The cat's name is ${catName}.`;
+para2.textContent = `The cat's color is ${cat.color}.`;
+
+section.appendChild(para1);
+section.appendChild(para2);
+
+// MDN JavaScript Objects Basics Assessment 2
+let bandInfo;
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+let para1 = document.createElement('p');
+para1.textContent = bandInfo;
+section.appendChild(para1);
+
+// MDN JavaScript Objects Basics Assessment 3
+let cat = {
+  name: 'Bertie',
+  breed: 'Cymric',
+  color: 'white',
+  greeting: function() {
+    console.log('Meow!');
+  }
+};
